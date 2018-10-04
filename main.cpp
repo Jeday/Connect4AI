f06240a5842a5bad992100aad1fd1183a5d2b516
unknown <iolanta.paly@gmail.com>
2018-10-04 15:28:05 +0300

    add do_move/undo_move, validate_move and is_won

--- main.cpp
+++ main.cpp
@@ -16,16 +16,115 @@ public:
     std::vector<signed char> board;
 
 
-    bool validate_move(int colom){} // проверяет что можно совершить ход в  colom, т.е. столбец не переполнен
+	game() {
+		board.resize(board_size, NONE);
+	}
+
+    bool validate_move(int colom){    // проверяет что можно совершить ход в  colom, т.е. столбец не переполнен
+		return (board[board_size - COLOMS + colom] == NONE);
+	} 
+
     int calculate_heuristics(){} // высчитывает эвристику для текущего располжения доски
-    signed char do_move(bool our_move,char colom){} // применяет ход(меняет доску) для нас(our_move == true) или для пртивника, возвращает AI(выграли мы),HUMAN(выграл Пучкин),NONE(не выигрщный ход)
-    void undo_move(bool our_move,char colom){}   //  отменят ход(меняет доску в обртное от хода положение) по сути просто убирает верхнюю фишку в строке
+    signed char do_move(bool our_move,char colom){   // применяет ход(меняет доску) для нас(our_move == true) или для противника, возвращает AI(выграли мы),HUMAN(выграл Пучкин),NONE(не выигрышный ход)
+		int i;
+		for (i = ROWS - 1; i >= 0; --i) {
+			if (board[i * COLOMS + colom] != NONE)
+				break;
+		}
+		++i;
+		board[i * COLOMS + colom] = our_move ? AI : HUMAN;
+
+		if (is_won(i * COLOMS + colom))
+			return our_move ? AI : HUMAN;
+		else
+			return NONE;
+	}
+
+private:
+	bool is_won(int ind) {
+		signed char type_pl = board[ind];
+		int cnt = 0;
+
+		for (int i = ind + 1; i%COLOMS != 0; ++i) {           // right horizontal
+			if (board[i] == type_pl)
+				++cnt;
+			else
+				break;
+		}
+		for (int i = ind - 1; i%COLOMS != COLOMS - 1; --i) {   // left horizontal
+			if (board[i] == type_pl)
+				++cnt;
+			else
+				break;
+		}
+		if (cnt >= 3)
+			return true;
+		cnt = 0;
+
+		for (int i = ind + COLOMS; i < board_size; i += COLOMS) {   // up
+			if (board[i] == type_pl)
+				++cnt;
+			else
+				break;
+		}
+		for (int i = ind - COLOMS; i >= 0; i -= COLOMS) {     // down
+			if (board[i] == type_pl)
+				++cnt;
+			else
+				break;
+		}
+		if (cnt >= 3)
+			return true;
+		cnt = 0;
+
+		for (int i = ind + COLOMS + 1; i < board_size && i%COLOMS != 0; i += COLOMS + 1) {   // up right diagonal
+			if (board[i] == type_pl)
+				++cnt;
+			else
+				break;
+		}
+
+		for (int i = ind - COLOMS - 1; i >= 0 && i%COLOMS != COLOMS - 1; i -= COLOMS + 1) {    // down left diagonal
+			if (board[i] == type_pl)
+				++cnt;
+			else
+				break;
+		}
+		if (cnt >= 3)
+			return true;
+		cnt = 0;
+
+		for (int i = ind - COLOMS + 1; i >= 0 && i%COLOMS != 0; i -= COLOMS - 1) {     // down right diagonal
+			if (board[i] == type_pl)
+				++cnt;
+			else
+				break;
+		}
+
+		for (int i = ind + COLOMS - 1; i < board_size && i%COLOMS != COLOMS - 1; i += COLOMS - 1) {   // up left diagonal
+			if (board[i] == type_pl)
+				++cnt;
+			else
+				break;
+		}
+		if (cnt >= 3)
+			return true;
+		cnt = 0;
+	}
+
+
+public:
+
+    void undo_move(bool our_move,char colom){  //  отменяет ход(меняет доску в обратное от хода положение) по сути просто убирает верхнюю фишку в строке
+		int i;
+		for (i = ROWS - 1; i >= 0; --i) {
+			if (board[i * COLOMS + colom] != NONE)
+				break;
+		}
+		board[i * COLOMS + colom] = NONE;
+	}   
     void print_board(){} // КРАСИВО печатает доску на консоль
 
-    game(){
-        board.resize(board_size,NONE);
-
-    }
 
 };
 
